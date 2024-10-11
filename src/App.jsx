9de454312cf33fb076b6b372ee392cbfb45df7bd
ozdenatabey <ozdenatabey44@gmail.com>
2024-10-11 13:51:34 +0300

    added Page.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import Header from "./component/Header";
-import AboutMe from "./component/AboutMe";
-import Experience from "./component/Experience";
-import References from "./component/References";
-import SideMenu from "./component/SideMenu";
 import { useEffect, useRef, useState } from "react";
 import ColorPicker from "./component/ColorPicker";
 import LanguageSelect from "./component/LanguageSelect";
 import Form from "./component/Form";
+import Page from "./component/Page";
 
 function App() {
   // DİL SEÇİMİ
@@ -45,36 +41,20 @@ function App() {
       <main className=" flex justify-between">
         <section id="leftSide" className="w-2/5">
           <Form theme={selectedTheme} language={language} />
+          <div className="flex justify-center my-4">
+            <button className="bg-neutral-700 text-white font-semibold text-lg w-36 py-3 rounded-lg hover:bg-neutral-800 active:scale-[0.97]">
+              {language === "en" ? "Download" : "İndir"}
+            </button>
+          </div>
         </section>
         <section id="rightSide" className="w-3/5">
           <div className="grid place-items-center">
-            <div
-              id="page1"
-              className={`bg-${selectedTheme}-base m-6 h-[297mm] w-[210mm] border-2 border-black rounded-lg overflow-hidden`}
-            >
-              <div className="flex">
-                <div className="w-2/6">
-                  <SideMenu theme={selectedTheme} language={language} />
-                </div>
-                <div ref={myRef} className="w-4/6 p-4 space-y-6">
-                  <Header theme={selectedTheme} />
-                  <AboutMe theme={selectedTheme} language={language} />
-                  <Experience theme={selectedTheme} language={language} />
-                </div>
-              </div>
-            </div>
-            {pageCount === 2 && (
-              <div
-                id="page2"
-                className={`bg-${selectedTheme}-primary mb-6 h-[297mm] w-[210mm] grid place-items-center border-2 border-black rounded-lg overflow-hidden`}
-              >
-                <div
-                  className={`bg-${selectedTheme}-base w-[200mm] h-[287mm] rounded-lg`}
-                >
-                  <References />
-                </div>
-              </div>
-            )}
+            <Page
+              theme={selectedTheme}
+              language={language}
+              myRef={myRef}
+              pageCount={pageCount}
+            />
           </div>
         </section>
       </main>
